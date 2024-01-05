9166830b543f377ac81d7e2ce511cc8c7807f3ef
ersonp <ersonpereiracr7@gmail.com>
2024-01-05 18:49:20 +0530

    Update expandable header styles

--- yew-web/src/components/exp_table_header.rs
+++ yew-web/src/components/exp_table_header.rs
@@ -38,7 +38,7 @@ impl Component for ExpandableHeader {
         html! {
             <div class="w-full mx-auto">
                 <div class="bg-background-50 rounded">
-                    <div class="transition-all duration-500 ease-in-out">
+                    <div class="">
                         <button  class="flex items-center justify-between w-full p-3 font-medium rtl:text-left" onclick={ctx.link().callback(|_| ExpandableHeaderState::Toggle)}>
                             <span class="flex items-center text-text-950"> {"Total: 5,00,000"}</span>
                                 <svg class="w-7 text-text-950" fill="currentColor" viewBox="0 0 24 24">
@@ -46,7 +46,7 @@ impl Component for ExpandableHeader {
                                 18V15H20V18H23V20H20V23H18V20H15V18H18Z" />
                             </svg>
                         </button>
-                        <div class={if self.open { "max-h-full transition-all duration-500 ease-in-out overflow-hidden" } else { "max-h-0 transition-all duration-500 ease-in-out overflow-hidden" }}>
+                        <div class={if self.open { "max-h-[1500px] overflow-hidden transition-all duration-100 ease-in-out" } else { "max-h-0 overflow-hidden transition-all duration-100 ease-in-out" }}>
                             <p class="w-full p-4 text-text-950 text-base bg-background-50 rounded-b">
                                 <div class="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
                                     <CreateInvForm create_investment={self.create_investment.clone()} />
