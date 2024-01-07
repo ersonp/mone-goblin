32db75d71f5a13c13399c52bdfa2c8d3c0bbdc22
ersonp <ersonpereiracr7@gmail.com>
2024-01-07 14:02:47 +0530

    Add Documentation to mod.rs

--- yew-web/src/components/mod.rs
+++ yew-web/src/components/mod.rs
@@ -1,6 +1,19 @@
+//! This module contains various submodules related to the investment feature.
+
+/// This module contains the form used to create a new investment.
 pub mod create_inv_form;
+
+/// This module contains the form used to edit an existing investment.
 pub mod edit_inv_form;
+
+/// This module contains the table header for the investment list.
 pub mod exp_table_header;
+
+/// This module contains the individual investment item component.
 pub mod inv_item;
+
+/// This module contains the list of investments.
 pub mod inv_list;
+
+/// This module contains the switcher component.
 pub mod switcher;
